c7346590d01f3aaf0bf8fc05ee25165b1ba117cc
Unknown <trex1883@gmail.com>
2018-01-22 08:43:05 -0700

    First version of assignment 1

---
+++ Daniel-Purrington-assign1/color.js
@@ -0,0 +1,8 @@
+
+
+var firstName, lastName, favColor;
+firstName = prompt('What is your first name');
+lastName = prompt('last name?');
+favColor = prompt('What is your favorite color?');
+alert('His first name is ' + firstName);
+alert('full name is ' + firstName + ' ' + lastName + ' and your favorite color is ' + favColor);
